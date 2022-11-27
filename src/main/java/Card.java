e526d64cd318f09a0fcf870a31b2212639aeeae4
bossNilac <calin.baculescu@gmail.com>
2022-11-27 14:37:13 +0200

    Version 1.0

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -5,6 +5,8 @@ public class Card {
     public static Card card;
     public String card_symbol;
     public String card_number;
+    public String card_color;
+    public String[] properties;
 
     public Card(String card_symbol, String card_number) {
         this.card_symbol = card_symbol;
@@ -21,18 +23,29 @@ public class Card {
 
     public static void genCard() {
         card = new Card(Main.symbol[new Random().nextInt(Main.symbol.length)], Main.number[new Random().nextInt(Main.number.length)]);
+        Main.isJoker=false;
         switch (card.getSymbol()) {
-            case "♥":Main.guess_symbol ="heart";  break;
-            case "♦":Main.guess_symbol ="diamond"; break;
-            case "♠":Main.guess_symbol ="spade"; break;
-            case "♣":Main.guess_symbol ="club"; break;
-            case "J":Main.guess_symbol ="joker";Main.isJoker = true; break;
+            case "♥":Main.guess_symbol ="heart";card.card_color ="red";  break;
+            case "♦":Main.guess_symbol ="diamond";card.card_color ="red"; break;
+            case "♠":Main.guess_symbol ="spade";card.card_color ="black"; break;
+            case "♣":Main.guess_symbol ="club";card.card_color ="black"; break;
+            case "J":Main.guess_symbol ="joker";Main.isJoker = true;card.card_color ="joker"; card.card_number=""; break;
             default:
                 System.out.println("wrong input");
                 break;
         }
-        System.out.println("Card has been generated");
+        if(!Main.isJoker) {
+            if (Integer.parseInt(card.card_number) < 10 && Integer.parseInt(card.card_number) >= 2) {
+                card.properties = new String[]{card.card_color, "2-9"};
+            }
+            if (Integer.parseInt(card.card_number) <= 14 && Integer.parseInt(card.card_number) >= 11) {
+                card.properties = new String[]{card.card_color, "11-14"};
+            }
+        }else{
+            card.properties = new String[]{card.card_color, ""};
+        }
 
+        System.out.println("Card has been generated =>" +card.card_number +"=> "+card.card_color+"joker:"+Main.isJoker);
     }
 
     public static void drawCard(Card card){
@@ -46,6 +59,7 @@ public class Card {
                     System.out.print("*");
                 else if (i == 5 && j == 6)
                     System.out.print(card.getSymbol());
+                else if (((i==2 &&j==3)|| (i == 8 && j == 10))&&Main.isJoker) System.out.print(" ");
                 else if(!Main.isJoker && card.getNumber().length()>1){
                     if ((i == 2 && j == 3) || (i == 8 && j == 9)) {
                         System.out.print(card.getNumber().charAt(0));
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,50 +1,193 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class Main {
+    static double balance;
     static boolean isJoker=false;
+    static boolean isColor;
+    static boolean isNumber;
     static String guess_symbol;
     static boolean flag = true;
-    public static final String[] symbol = {"♥", "♦", "♠", "♣", "J"};
-    public static final String[] number = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "1"};
+    public static final String[] symbol = {"♥", "♦", "♠", "♣","♥", "♦", "♠", "♣","♥", "♦", "♠", "♣" ,"♥", "♦", "♠", "♣","J"};
+    public static final String[] number = {"2", "3", "4", "5", "6", "7", "8", "9", "11", "12", "13", "14"};
+    static String[] userGuess;
 
     public static void main(String[] args) {
+        loadProperties();
+
         Card.genCard();
         while (flag) {
-            Scanner sc = new Scanner(System.in);
-            System.out.println("choose symbol:");
-            String symbol_input = sc.nextLine().toLowerCase();
+            print_rules();
 
-            System.out.println("choose number:");
-            String number_input = sc.nextLine();
+            Scanner bet_scanner = new Scanner(System.in);
+            System.out.println("Enter bet:");
+            double bet = bet_scanner.nextDouble();
+            double temp = balance;
 
-            if (number_input.equals(Card.card.getNumber())&& symbol_input.equals(Card.card.getSymbol())) {
-                System.out.println("you guessed both");
-                System.out.println("--------------------");
-            }else if(number_input.equals(Card.card.getNumber())&& !symbol_input.equals(Card.card.getSymbol())){
-                System.out.println("you guessed the number");
-                System.out.println("--------------------");
-            }else if(!number_input.equals(Card.card.getNumber()) && symbol_input.equals(Card.card.getSymbol())){
-                System.out.println("you guessed the number");
-                System.out.println("--------------------");
-            }else if(!number_input.equals(Card.card.getNumber())&&!symbol_input.equals(Card.card.getSymbol())){
-                System.out.println("you did not guess:/");
-                System.out.println("--------------------");
+            isColor=false;
+            isNumber=false;
+            Scanner type_scanner = new Scanner(System.in);
+            System.out.println("Enter bet type:");
+            String bet_type ;
+            bet_type = type_scanner.nextLine();
+            switch (bet_type){
+                case "color":isColor=true; System.out.println("Your final bet:"+bet);  break;
+                case "number":isNumber=true; System.out.println("Your final bet:"+bet); break;
+                case "double":isColor=true;isNumber=true;System.out.println("Your final bet:"+bet*2); break;
             }
 
+            Scanner sc = new Scanner(System.in);
+            String color_input = null,number_input = null;
+        if(isColor&&isNumber){
+            color_input=loadDialogColor(sc);
+            number_input=loadDialogNumber(sc);
+        }
+        else if(isColor){
+            color_input=loadDialogColor(sc);
+            number_input="null";
+        }else if(isNumber){
+            color_input="null";
+            number_input=loadDialogNumber(sc);
+        }
+
+            userGuess = new String[]{color_input, number_input};
+
+            if (userGuess[0].equals(Card.card.properties[0])) {
+                if (!isJoker) {
+                    System.out.println("You guessed the color");
+                    System.out.println("You won : "+bet*2);
+                    balance =balance+
+                            bet*2;
+                } else {
+                    System.out.println("You guessed the joker");
+                    balance =balance + bet *24;
+                    System.out.println("You won : "+bet*24);
+                }
+            }else{
+                if(isColor) {
+                    System.out.println("(1)You lost: " + bet);
+                    balance = balance - bet;
+                }
+            }
+
+            if(userGuess[1].equals(Card.card.properties[1])||Card.card.properties[1].contains(userGuess[1])||(userGuess[1].equals("11&14")&&Card.card.properties[1].equals("11-14"))){
+                switch (number_input) {
+                    case "2-9":
+                        balance = balance + bet * 1.5;
+                        break;
+                    case "11-14":
+                        balance = balance + bet * 3;
+                        break;
+                    case "11&14":
+                        if (Card.card.properties[1].contains("11") || Card.card.properties[1].contains("14"))
+                            balance = balance + bet * 6;
+                        break;
+                    case "11":
+                        if (Card.card.properties[1].contains(number_input)) balance = balance + bet * 12;
+                        break;
+                }
+                if (!isJoker) {
+                    System.out.println("You guessed the number");
+                    if(isColor) {
+                        System.out.println("You won : " + ((balance - bet*2 - temp)));
+                    }else{
+                        System.out.println("You won : " + ((balance - bet - temp)));
+                    }
+                }
+            }else{
+                if(isNumber) {
+                    System.out.println("(2)You lost: " + bet);
+                    balance = balance - bet;
+                }else {
+                    if (isJoker && isNumber) {
+                        System.out.println("(2)You lost: " + bet);
+                        balance = balance - bet;
+                    }
+                }
+            }
+
+            System.out.println("balance :"+balance);
             Card.drawCard(Card.card);
-                System.out.println("continue?");
-                String command = sc.nextLine();
-                if (command.equals("yes")) {
-                    Card.genCard();
-                }
-                if (command.equals("no")) {
-                    flag = false;
-                    System.out.println("Thanks for choosing our service");
-                }
-                if (command.equals("0")) {
-                    System.exit(0);
-                }
+
+            storeProperties(balance);
+
+            System.out.println("Balance:"+balance);
+            System.out.println("continue?");
+            String command = sc.nextLine();
+            if (command.equals("yes")) {
+                Card.genCard();
+            }
+            if (command.equals("no")) {
+                flag = false;
+                System.out.println("Thanks for choosing our service");
             }
         }
     }
+
+
+    private static void print_rules() {
+        System.out.println("Rules of HiLo:");
+        System.out.println("Choose the color:black or red ( X 2 bet) or Joker ( X 24 bet )");
+        System.out.println("Choose numbers : range 2-9 ( X 1.5 bet) or 11-12-13-14 ( X 3 bet )  or 11 & 14 ( X 6 bet ) or 11 ( X12 bet )");
+
+        System.out.println("Types of bets:");
+        System.out.println("color bet , number bet , ");
+        System.out.println("double bet(takes double your bet  ,bet on color and number range ");
+        System.out.println("You will be asked which bet you would like");
+
+        System.out.println("User input rules :");
+        System.out.println("Colors: black , red , joker");
+        System.out.println("Numbers: 2-9 , 11-14 , 11&14 , 11");
+        System.out.println("Bets:color , number , double");
+        System.out.println();
+        System.out.println();
+    }
+
+    private static void loadProperties()  {
+        String data = null;
+        try {
+            data = new String(Files.readAllBytes(Paths.get("src/main/resources/prop/balance.hilo")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("data:"+data);
+        balance= Double.parseDouble(data);
+    }
+    private static void storeProperties(double balance)  {
+        try
+        {
+            String filename= "src/main/resources/prop/balance.hilo";
+            FileWriter fw = new FileWriter(filename,false);
+            fw.write(""+balance);
+            fw.close();
+        }
+        catch(IOException ioe)
+        {
+            System.err.println("IOException: " + ioe.getMessage());
+        }
+    }
+
+    static String loadDialogColor(Scanner sc){
+        System.out.println("choose color:");
+        String color_input = sc.nextLine().toLowerCase();
+        return color_input;
+    }
+
+    static String loadDialogNumber(Scanner sc){
+        String number_input=null;
+        if (!isJoker) {
+            System.out.println("choose numbers: ");
+            number_input = sc.nextLine();
+        } else {
+                if(isNumber&&isJoker){
+                    number_input = "xxx";
+                }
+        }
+
+        return number_input;
+    }
+}
+
 
